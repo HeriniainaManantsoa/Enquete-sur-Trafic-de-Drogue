1e874e3ded64ae0fce0c7fc7e4f43102de63d622
HeriniainaManantsoa <ramanantsoaheriniaina89@gmail.com>
2026-06-02 04:09:44 +0300

    Add files via upload

--- ComDetection/main.py
+++ ComDetection/main.py
@@ -4,21 +4,23 @@ import Generation
 import SaveData
 import math
 
-n = 50
-p = math.log(n) / n
-G = Generation.Generateur(n,p)
-GPrime = G.get_G_prime()
 
-SaveData.save_data(GPrime,GPrime.V,GPrime.E)
+if __name__ == "__main__":
+    n = 50
+    p = math.log(n) / n
+    G = Generation.Generateur(n,p)
+    GPrime = G.get_G_prime()
 
-classificateur = Classification.Classificateur()
-classificateur.V = GPrime.V
-classificateur.E = GPrime.E
-cliques = classificateur.rechercher_cliques()
-for clique in cliques:
-    for e in clique:
-        print (e.id)
-    print ()
+    #SaveData.save_data(GPrime,GPrime.V,GPrime.E)
 
-affichageGPrime = Interface.Interface(GPrime)
-affichageGPrime.create_interface()
+    classificateur = Classification.Classificateur()
+    classificateur.V = GPrime.V
+    classificateur.E = GPrime.E
+    cliques = classificateur.rechercher_cliques()
+    for clique in cliques:
+        for e in clique:
+            print (e.id)
+        print ()
+
+    affichageGPrime = Interface.Interface(GPrime)
+    affichageGPrime.create_interface()
